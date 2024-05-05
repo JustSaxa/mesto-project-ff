a9745af2c9eace1d00efc5fe14cb9f5d4e785cd1
Saxa <just.saxa@gmail.com>
2024-05-05 17:49:59 +0500

    firs commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function addCard(cardCreate, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
+    cardElement.querySelector('.card__title').textContent = cardCreate.name;
+    cardElement.querySelector('.card__image').src = cardCreate.link;
+    cardElement.querySelector('.card__image').alt = cardCreate.name;
+
+    deleteButton.addEventListener('click', () => {
+        deleteCard(cardElement);
+      });
+
+      return cardElement;
+  };
 // @todo: Функция удаления карточки
-
+function deleteCard(card) {
+    card.remove();
+  }
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((function (element) {
+    cardList.append(addCard(element, deleteCard));
+  }))
+
+
+  
+
+  
