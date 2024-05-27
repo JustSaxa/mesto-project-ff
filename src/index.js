377120a00d3ee38305a7d1675eac548264d2a2d7
Saxa <just.saxa@gmail.com>
2024-05-27 19:14:41 +0500

    место 2.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import './pages/index.css';
+import {initialCards} from './scripts/cards';
+
+
+
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
